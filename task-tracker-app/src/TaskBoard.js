d98241e81a5de5d8ca9074cf91cfe2fec5092299
FBetulDemir <fatmabetuld@gmail.com>
2024-09-09 15:59:31 +0200

    Initial commit

---
+++ task-tracker-app/src/TaskBoard.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const TaskBoard = () => {
+  const [tasks, setTasks] = useState([]);
+  const [textInput, setTextInput] = useState("");
+  const [deleteTask, setDeleteTask] = useState("");
+  const [updateTask, setUpdateTask] = useState("");
+
+  return <></>;
+};
+
+export default TaskBoard;
